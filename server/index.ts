0dcbea4a776cbb196a18d0f70c6542e767c99e5b
aiverse <abhiaiverse1@gmail.com>
2026-02-10 17:53:45 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (_req, res) => {
   res.send("SEMANTYX v1 is running");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
